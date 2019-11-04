ec7525da0b4483bd35cb3879e7487b62d6cc56f8
chrislayco <57198098+chrislayco@users.noreply.github.com>
2019-11-04 09:42:10 -0800

    Add files via upload

--- models.py
+++ models.py
@@ -1,4 +1,29 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	
+	def __str__(self):
+		return self.question_text
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+	
+
+	def __str__(self):
+		return self.choice_text
+
+
+
 
 class Team(models.Model):
 	# auto-gen id is primary key (?)
@@ -17,7 +42,7 @@ class User(models.Model):
 	#team_id = models.int() #need more parameters?
 
 class Progress(models.Model):
-	team = models.ForeignKey(Team)
-	activity_1_prog = models.int()
-	activity_2_prog = models.int()
-	activity_3_prog = models.int()
+	team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
+	activity_1_prog = models.IntegerField()
+	activity_2_prog = models.IntegerField()
+	activity_3_prog = models.IntegerField()
