009f14c1d55bfb72982f420dcdb74c15727d158b
jrmajors <jrmajors@users.noreply.github.com>
2019-11-04 09:30:35 -0800

    Add files via upload

--- models.py
+++ models.py
@@ -3,14 +3,14 @@ from django.db import models
 class Team(models.Model):
 	# auto-gen id is primary key (?)
 	#team_id already gen (?)
-	leader = models.ForeignKey(User)
-	member_2 = models.ForeignKey(User)
-	member_3 = models.ForeignKey(User)
+	leader = models.CharField(max_length=50)
+	member_2 = models.CharField(max_length=50)
+	member_3 = models.CharField(max_length=50)
 	team_name = models.CharField(max_length=50)
 
 class User(models.Model):
 	# auto-gen id is primary key (?)
-	team = models.ForeignKey(Team)
+	team = models.CharField(max_length=50)
 	email = models.CharField(max_length=50)
 	hashed_password = models.CharField(max_length=50)
 	name = models.CharField(max_length=50)
