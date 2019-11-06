72f773f75e5eed4a5d254062157817d83ea6dce6
jrmajors <jrmajors@users.noreply.github.com>
2019-11-06 09:41:25 -0800

    Add files via upload

--- models.py
+++ models.py
@@ -8,6 +8,9 @@ class Team(models.Model):
 	member_2 = models.CharField(max_length=50, blank=True)
 	member_3 = models.CharField(max_length=50, blank=True)
 
+	def __str__(self):
+		return self.team_name
+
 class User(models.Model):
 	# auto-gen id is primary key (?)
 	email = models.CharField(primary_key=True, max_length=50)
@@ -16,8 +19,14 @@ class User(models.Model):
 	#name = models.CharField(max_length=50)
 	#team_id = models.int() #need more parameters?
 
+	def __str__(self):
+		return self.email
+
 class Progress(models.Model):
 	team = models.ForeignKey(Team, on_delete=models.SET_NULL, blank=True)
 	activity_1_prog = models.IntegerField()
 	activity_2_prog = models.IntegerField()
 	activity_3_prog = models.IntegerField()
+
+	def __str__(self):
+		return self.team
