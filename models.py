6fadcd685b37a8cafc38cda441c7daf55c3a08eb
jrmajors <jrmajors@users.noreply.github.com>
2019-11-06 09:29:26 -0800

    Add files via upload

--- models.py
+++ models.py
@@ -5,19 +5,19 @@ class Team(models.Model):
 	#team_id already gen (?)
 	team_name = models.CharField(primary_key=True, max_length=50)
 	leader = models.CharField(max_length=50)
-	member_2 = models.CharField(max_length=50, null=True)
-	member_3 = models.CharField(max_length=50, null=True)
+	member_2 = models.CharField(max_length=50, blank=True)
+	member_3 = models.CharField(max_length=50, blank=True)
 
 class User(models.Model):
 	# auto-gen id is primary key (?)
 	email = models.CharField(primary_key=True, max_length=50)
 	password = models.CharField(max_length=50)
-	team = models.CharField(max_length=50, null=True)
+	team = models.CharField(max_length=50, blank=True)
 	#name = models.CharField(max_length=50)
 	#team_id = models.int() #need more parameters?
 
 class Progress(models.Model):
-	team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
+	team = models.ForeignKey(Team, on_delete=models.SET_NULL, blank=True)
 	activity_1_prog = models.IntegerField()
 	activity_2_prog = models.IntegerField()
 	activity_3_prog = models.IntegerField()
